4bf4a5d46f0d6cb514c5869f5d0e8f2da241c6b3
Taehyun-Lee <taehyunlee@hotmail.ca>
2017-03-05 00:03:13 -0500

    Search Update
    Added search.php
    Currently not functional.

--- search.php
+++ search.php
@@ -1,17 +1,4 @@
-<html lang="en">
-    <head>
-        <meta charset='utf-8'>
-        <title>Orbit: Dynamic Library</title>
-        <script src='movement.js'></script>
-        
-        <link rel="stylesheet" href="main.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script>
-         var display_str = [];
-        
-        <?php
+<?php
         
         require_once('config.php');
         
@@ -26,41 +13,68 @@
         if(!$db_selected) {
             die('Can\'t use ' . DB_NAME . ': ' .mysql_error());
         }
-
-   
-        $sql = "SELECT * FROM uploads";
-        $checkval = false;
-        
-        
         $max_date = $_POST['max_date'];
         $min_date = $_POST['min_date'];
+        
+        $sql = "SELECT * FROM uploads WHERE date BETWEEN $min_date AND $max_date AND type = $type";
+        
         $type = $_POST['type'];
         
         
         $results = mysql_query($sql);
-        
+        $results_arr = array();
         if (!$results) {
             die('Invalid query: ' . mysql_error());
         }
         
-        while($result = mysql_fetch_array($results)) {
-            if($result['type'] == $type){
-                echo $result['title'] + "\n" + $result['content'];
-                
-            }
-                
-            
-        }
-        if($username != "") {
-        echo 'No such email and password combination found';
+        while($result = mysql_fetch_array($results)) {    
+            array_push($results_arr, $result); 
         }
         mysql_close();
         ?>
+<html lang="en">
+    <head>
+        <meta charset='utf-8'>
+        <title>Orbit: Dynamic Library</title>
+        <script src='movement.js'></script>
+        
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script>
+        
+         var display_strs = <?php echo json_encode($results_arr) ?>;
+         
+         var display_arr = JSON.parse(display_strs);
+        var element = document.getElementById("result_field");
+        for(i = 0; i < display_arr.length; i++){
+            var title = document.createElement("h2");
+            var para = document.createElement("p");
+            var node1 = document.createTextNode(display_arr[i]['title']);
+            var node2 = document.createTextNode(display_arr[i]['content']);
+            title.appendChild(node1);
+            para.appendChild(node2);
+            element.appendChild(title);
+            element.appendChild(para);
+                
+        }
         </script>
     </head>
     <body>
-        
-        
-       
+        <div>
+            <form action="search.php" method="post">
+                <input type="datetime" name="min_time"><br>
+                <input type="datetime" name="max_time"><br>
+                <select name="type">
+                    <option value="quote">Quote of the day</option>
+                    <option value="meme">Meme of the day</option>
+                    <option value="word">Word of the day</option>
+                </select>
+                <input type="submit" value="Search">
+            </form>
+        </div>
+        <div id="result_field">
+        </div>
     </body>
 </html>
