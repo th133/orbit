594bbc50d35ed48287200f6616fe434fe73fc19f
Taehyun-Lee <taehyunlee@hotmail.ca>
2017-03-08 16:59:06 -0500

    more changes

--- result.php
+++ result.php
@@ -2,40 +2,38 @@
         
         require_once('config.php');
         
-       $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-
-        if(!$link) {
-            die('Could not connect: '. mysql_error());
-        }
-
-        $db_selected = mysql_select_db(DB_NAME, $link);
+       $db_selected = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
         if(!$db_selected) {
-            die('Can\'t use ' . DB_NAME . ': ' .mysql_error());
+            die('Could not connect: '. mysqli_connect_error());
         }
-        $max_date = $_POST['max_year'].'-'. $_POST['max_month'].'-'.$_POST['max_date'];
-        $min_date = $_POST['min_year'].'-'. $_POST['min_month'].'-'.$_POST['min_date'];;
+
+
+        $max_date = '#'.$_POST['max_year'].'/'. $_POST['max_month'].'/'.$_POST['max_date'].'#';
+        $min_date = '#'.$_POST['min_year'].'/'. $_POST['min_month'].'/'.$_POST['min_date'].'#';;
+        
         $type = $_POST['type'];
-        echo $max_date. $min_date;
+        
+        $num_show = $_POST['num_show'];
     
-        $sql = "SELECT * FROM uploads WHERE type = '$type'";
+        $sql = "SELECT title,content FROM uploads WHERE type = '$type'";
         //WHERE date BETWEEN $min_date AND $max_date AND type = $type"
         
         
         
         
-        $results = mysql_query($sql);
+        $results = mysqli_query($db_selected, $sql);
         $results_arr = array();
         if (!$results) {
-            die('Invalid query: ' . mysql_error());
+            die('Invalid query: ' . mysqli_connect_error());
         }
         
-        while($result = mysql_fetch_array($results)) {
+        while($result = mysqli_fetch_array($results)) {
             $new_result = array($result['title'], $result['content']);
             array_push($results_arr, $new_result);
         }
         $json_result = json_encode($results_arr);
-        mysql_close();
+        mysqli_close($db_selected);
 ?>
 <html lang="en">
     <head>
@@ -48,6 +46,10 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body onload="show_results()">
+        <div id="button_field">
+            <a id="show_btn" onclick="increment_show('prev')">[prev]</a>
+            <a id="show_btn" onclick="increment_show('next')">[next]</a>
+        </div>
         <div id="result_field">
         </div>
         <!-- The Modal -->
@@ -60,7 +62,15 @@
         <script>
             
             var display_strs = <?php echo $json_result ?>;
-        var result_width = 20;
+            var max_length = display_strs.length;
+            var result_width = 20;
+            var start_show = 0;
+            var num_show = <?php echo $num_show ?>;
+            var last_show = 0;
+            if(num_show < max_length){
+                last_show = num_show;
+            }
+            var stop_myMove = false;
             
         function reset_result_div_size(){
             var result_div = document.getElementById("result_field");
@@ -73,12 +83,10 @@
         function show_results(){
             
             
-            for(var i = 0; i < display_strs.length; i++){
+            for(var i = start_show; i < last_show; i++){
                 var element = document.getElementById("result_field");
                 reset_result_div_size();
-                var i_str = i.toString();
-                
-                var space = document.createElement("a");
+                var space = document.createElement("h3");
                 space.id = 'animate';
                 space.style.width = result_width;
                 space.style.height = result_width;
@@ -96,29 +104,32 @@
                 element.appendChild(space);
                 
             }
-            myMove();
-            location.href = '#result_field';
+            my_move();
         }
+            
         
-            function myMove() {
-            var elem = document.getElementsByTagName("a");
+            function my_move() {
+            var elem = document.getElementsByTagName("h3");
+            stop_myMove = false;
+            reset_result_div_size();
             var elem_pos = [];
             var elem_spd = [];
+            var elem_not_passed = [];
             var spd_min = 1;
-            var spd_max = 3;
-            reset_result_div_size();
+            var spd_max = 4;
             var width_c = window.innerWidth;
             var height_c = Math.round(window.innerHeight * (9 / 10));
             var len = elem.length;
-            var ratio = 1.0;
+            var ratio = 1;
             if(len > 1){
                 ratio = (len - 1) / len;
             }
-            var check_len = Math.round((width_c - (result_width * 2)) * ratio);
+            var check_len = Math.round((width_c - result_width * 2) * ratio);
             
-            for(i = 0; i < len; i++){
+            for(var i = 0; i < len; i++){
                 elem_spd[i] = Math.floor(Math.random() * (spd_max - spd_min)) + spd_min;
                 elem_pos[i] = width_c;
+                elem_not_passed[i] = true;
                 elem[i].style.top = Math.round(height_c * Math.random());
                 if(elem_spd[i] == 1){
                     elem[i].style.backgroundColor = "#ffff80";
@@ -132,9 +143,9 @@
                 document.getElementById("modal_content").innerHTML = c[1].innerHTML;
                 };
                 
-            }
-            
-            var id = setInterval(frame, 5);
+            }   
+                
+            var id = setInterval(frame, 17);
             var moving = 1;
             function frame() {
                 for(var i = 0; i < moving; i++){
@@ -142,15 +153,40 @@
                         elem[i].style.left = width_c;
                         elem_pos[i] = window.innerWidth;
                     }else{
+                        if(stop_myMove){
+                            clearInterval(id);
+                        }
                         elem_pos[i] -= elem_spd[i];
                         elem[i].style.left = elem_pos[i] + 'px';
-                        if(elem_pos[i] == check_len && moving < len){
+                        if(elem_not_passed[i] && elem_pos[i] <= check_len && moving < len){
                             moving++;
+                            elem_not_passed[i] = false;
                         }
                     }
                 }
             }
         }
+            function increment_show(change){
+                if(change == "next"){
+                    if(last_show + num_show < max_length){
+                        last_show += num_show;
+                    }else{
+                        last_show = max_length;
+                    }
+                } else {
+                    if(start_show != 0){
+                        start_show -= num_show;
+                        last_show -= num_show;
+                    }
+                    
+                }
+                stop_myMove = true;
+                var field = document.getElementById("result_field");
+               /*while(field.hasChildNodes){
+                    field.removeChild(field.lastChild);
+                }*/
+                show_results();
+            }
             
             // Get the modal
             var modal = document.getElementById('myModal');
